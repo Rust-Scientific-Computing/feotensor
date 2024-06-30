dee5dd2468624a0056f5d3dcc3586b6aa65bc843
Angus Stewart <siliconlad@protonmail.com>
2024-06-30 20:32:26 +0100

    perf: add initial benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,12 @@ pub mod shape;
 pub mod storage;
 pub mod tensor;
 pub mod vector;
+
+// Re-export important structs
+pub use axes::Axes;
+pub use shape::Shape;
+pub use tensor::Tensor;
+pub use vector::Vector;
+pub use matrix::Matrix;
+pub use coordinate::Coordinate;
+pub use error::ShapeError;
